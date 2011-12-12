105aaf6bf892f097672c5383c08fe0a18cd6c24e
Mario Visic <mario@mariovisic.com>
2011-12-12 14:02:56 +0800

    Add blank gateway login details to the spec helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 require 'activemerchant-bpoint'
 require 'support/gateway_helpers'
 
-
 RSpec.configure do |config|
   config.include GatewayHelpers
 end
+
+GATEWAY_LOGIN    = ''
+GATEWAY_PASSWORD = ''
