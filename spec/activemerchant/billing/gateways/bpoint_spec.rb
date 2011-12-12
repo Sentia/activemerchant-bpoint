448a370b1fb4e841bfa15ed19d405b55dbe04dde
Mario Visic <mario@mariovisic.com>
2011-12-12 17:33:27 +0800

    tests for the initializer.

--- spec/activemerchant/billing/gateways/bpoint_spec.rb
+++ spec/activemerchant/billing/gateways/bpoint_spec.rb
@@ -2,52 +2,49 @@ require 'spec_helper'
 
 describe ActiveMerchant::Billing::BpointGateway do
   context 'on initialize' do
+    subject { ActiveMerchant::Billing::BpointGateway.new(:login => 'foo', :password => 'bar', :merchant_number => '7') }
+
     it 'sets the given username' do
-      pending
+      subject.instance_variable_get('@options')[:login].should == 'foo'
     end
 
     it 'sets the given password' do
-      pending
+      subject.instance_variable_get('@options')[:password].should == 'bar'
     end
 
-    it 'loads the given PEM SSL certificate file' do
-      pending
+    it 'sets the given merchant number' do
+      subject.instance_variable_get('@options')[:merchant_number].should == '7'
+    end
+
+    it 'should raise an exception if the username is missing' do
+      expect { ActiveMerchant::Billing::BpointGateway.new(:password => 'bar', :merchant_number => '4') }.to raise_error ArgumentError
+    end
+
+    it 'should raise an exception if the password is missing' do
+      expect { ActiveMerchant::Billing::BpointGateway.new(:login => 'foo', :merchant_number => '4') }.to raise_error ArgumentError
+    end
+
+    it 'should raise an exception if the merchant number is missing' do
+      expect { ActiveMerchant::Billing::BpointGateway.new(:login => 'foo', :password => 'bar') }.to raise_error ArgumentError
     end
   end
 
-
   describe '#purchase' do
-    it 'successfully performs a purchase on a valid mastercard credit card' do
+    it 'successfully performs a purchase on a valid credit card' do
       pending
     end
 
-    it 'successfully performs a purchase on a valid visa credit card' do
-      pending
-    end
-
-    it 'does not performs a purchase on an invalid mastercard credit card' do
-      pending
-    end
-
-    it 'does not performs a purchase on an invalid visa credit card' do
+    it 'does not performs a purchase on an invalid credit card' do
       pending
     end
   end
 
   describe '#store' do
-    it 'successfully stores a valid mastercard credit card' do
+    it 'successfully stores a valid credit card' do
       pending
     end
 
-    it 'successfully stores a valid visa credit card' do
-      pending
-    end
-
-    it 'does not store an invalid mastercard credit card' do
-      pending
-    end
-
-    it 'does not store an invalid visa credit card' do
+    it 'does not store an invalid credit card' do
       pending
     end
   end
