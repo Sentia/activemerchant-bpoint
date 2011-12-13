93c8627025e426c790fcfdf4af556fa6be55818e
Mario Visic <mario@mariovisic.com>
2011-12-13 11:26:13 +0800

    Using VCR for remote testing.

--- spec/activemerchant/billing/gateways/bpoint_spec.rb
+++ spec/activemerchant/billing/gateways/bpoint_spec.rb
@@ -28,46 +28,4 @@ describe ActiveMerchant::Billing::BpointGateway do
       expect { ActiveMerchant::Billing::BpointGateway.new(:login => 'foo', :password => 'bar') }.to raise_error ArgumentError
     end
   end
-
-  describe '#purchase' do
-    let(:gateway)             { ActiveMerchant::Billing::BpointGateway.new(:login => GATEWAY_LOGIN, :password => GATEWAY_PASSWORD, :merchant_number => GATEWAY_MERCHANT_NUMBER) }
-    let(:valid_credit_card)   { credit_card(:year => 2000) }
-    let(:invalid_credit_card) { credit_card(:year => 2010) }
-
-    context 'on a valid credit card' do
-      let(:response) { gateway.purchase(1000, valid_credit_card) }
-
-      it { should be_success }
-    end
-
-    context 'on an invalid credit card' do
-      let(:response) { gateway.purchase(1000, invalid_credit_card) }
-
-      it { should_not be_success }
-    end
-  end
-
-  describe '#store' do
-    it 'successfully stores a valid credit card' do
-      pending
-    end
-
-    it 'does not store an invalid credit card' do
-      pending
-    end
-  end
-
-  describe '#unstore' do
-    context 'when given a valid token' do
-      it 'removes a card cord from storage' do
-        pending
-      end
-    end
-
-    context 'when not given a valid token' do
-      it 'returns an error response' do
-        pending
-      end
-    end
-  end
 end
--- spec/integration/bpoint_spec.rb
+++ spec/integration/bpoint_spec.rb
@@ -6,7 +6,7 @@ describe ActiveMerchant::Billing::BpointGateway do
 
   context 'using invalid details' do
     let(:gateway)  { ActiveMerchant::Billing::BpointGateway.new(:login => 'does', :password => 'not_exist', :merchant_number => '8') }
-    let(:response) { gateway.purchase(100, credit_card, {}) }
+    let(:response) { VCR.use_cassette('invalid login') { gateway.purchase(100, credit_card, {}) } }
 
     it 'is not successful' do
       response.should_not be_success
@@ -14,6 +14,48 @@ describe ActiveMerchant::Billing::BpointGateway do
 
     it 'returns an invalid login status' do
       response.message.should include 'invalid login'
+    end
+  end
+
+  context 'making a purchase' do
+    let(:gateway)             { ActiveMerchant::Billing::BpointGateway.new(:login => GATEWAY_LOGIN, :password => GATEWAY_PASSWORD, :merchant_number => GATEWAY_MERCHANT_NUMBER) }
+    let(:valid_credit_card)   { credit_card(:year => 2000) }
+    let(:invalid_credit_card) { credit_card(:year => 2010) }
+
+    context 'on a valid credit card' do
+      let(:response) { gateway.purchase(1000, valid_credit_card) }
+
+      it { should be_success }
+    end
+
+    context 'on an invalid credit card' do
+      let(:response) { gateway.purchase(1000, invalid_credit_card) }
+
+      it { should_not be_success }
+    end
+  end
+
+  context 'storing a credit card' do
+    it 'successfully stores a valid credit card' do
+      pending
+    end
+
+    it 'does not store an invalid credit card' do
+      pending
+    end
+  end
+
+  context 'removing a credit card from storage' do
+    context 'when given a valid token' do
+      it 'removes a card cord from storage' do
+        pending
+      end
+    end
+
+    context 'when not given a valid token' do
+      it 'returns an error response' do
+        pending
+      end
     end
   end
 end
