1788a41363ff38068507303276cc5a546c9897f3
Mario Visic <mario@mariovisic.com>
2011-12-13 12:26:17 +0800

    Removed test credentials from the spec helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,12 +11,3 @@ VCR.config do |c|
   c.cassette_library_dir = File.dirname(__FILE__) + '/support/vcr_cassettes'
   c.stub_with :fakeweb
 end
-
-#
-# Fill in your gateway details below to test remotely (when not using VCR
-# prerecorded responses). This should only be neccessary when rerecording
-# existing actions or adding new remote tests
-#
-GATEWAY_LOGIN           = ''
-GATEWAY_PASSWORD        = ''
-GATEWAY_MERCHANT_NUMBER = ''
