dbcd6b7b103da2d15ab2dada4e83e10e95d5cc07
Mario Visic <mario@mariovisic.com>
2011-12-13 12:20:04 +0800

    Added blank credentials file.

---
+++ spec/test_credentials.rb
@@ -0,0 +1,11 @@
+#
+# Fill in your gateway details below to test remotely (when not using VCR
+# prerecorded responses). This should only be neccessary when rerecording
+# existing actions or adding new remote tests
+#
+# This file is ignored by git so you should't be able to accidently commit the
+# API details below.
+#
+GATEWAY_LOGIN           = ''
+GATEWAY_PASSWORD        = ''
+GATEWAY_MERCHANT_NUMBER = ''
